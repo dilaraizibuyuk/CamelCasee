60fe2b060cabdfe8d64e16570468c4741ce6639d
Dilara <74327986+dilaraizibuyuk@users.noreply.github.com>
2022-01-10 18:27:43 +0300

    Proje dosyası ekle.

---
+++ CamelCasee/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CamelCasee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("One123Two987".SplitCamelCase());
+            Console.WriteLine("aComputer".SplitCamelCase());
+            Console.WriteLine("casedWordHTTPWriter".SplitCamelCase());
+            Console.WriteLine("AppleComputer".SplitCamelCase());
+            Console.WriteLine("anIBM".SplitCamelCase());
+            Console.ReadKey();
+        }
+    }
+}
